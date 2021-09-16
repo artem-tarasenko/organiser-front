dfd563bbc67be59d632469fd52d805de3f32f450
Artem Tarasenko <artem.t.88@ya.ru>
2021-09-16 22:46:59 +1000

    refactoring st 2

--- src/components/NewHeader.jsx
+++ src/components/NewHeader.jsx
@@ -1,21 +1,55 @@
-import React from "react";
+import React, {useState} from "react";
+import styles from "./NewHeader.module.scss";
 
 import TuneIcon from '@material-ui/icons/Tune';
 import AttachMoneyIcon from '@material-ui/icons/AttachMoney';
+import EuroIcon from '@material-ui/icons/Euro';
 import PlaylistAddIcon from '@material-ui/icons/PlaylistAdd';
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 
-export default function NewHeader() {
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
 
+
+
+
+export default function NewHeader() {
 
 
     return <React.Fragment>
         <section className="header flex justify-around h-12 items-center bg-red-200">
             <AccountCircleIcon />
             <PlaylistAddIcon />
-            <AttachMoneyIcon />
+            <CurrencySelector />
             <TuneIcon />
         </section>
 
     </React.Fragment>
+}
+
+function CurrencySelector() {
+    const [age, setAge] = useState('RUB');
+
+    function handleChange(e) {
+        setAge(e.target.value);
+    };
+
+    return <>
+        <FormControl classes={styles.currency}>
+            <Select
+                inputProps={{ 'aria-label': 'Without label' }}
+                id="currency"
+                value={age}
+                onChange={handleChange}
+                label="Age"
+            >
+                <MenuItem value={"USD"}>USD</MenuItem>
+                <MenuItem value={"EUR"}>EUR</MenuItem>
+                <MenuItem value={"RUB"}>RUB</MenuItem>
+                <MenuItem value={"CAD"}>CAD</MenuItem>
+            </Select>
+        </FormControl>
+    </>
 }
--- src/components/TableHeading.jsx
+++ src/components/TableHeading.jsx
@@ -11,11 +11,17 @@ import Switch from '@material-ui/core/Switch';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import RepeatIcon from '@material-ui/icons/Repeat';
 
+import ExpandLessIcon from '@material-ui/icons/ExpandLess';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
 
 
 
 function TableHeading(props) {
     const [open, setOpen] = useState(false); //false - RUB, true - CAD
+
+    const [isExpanded, setIsExpanded] = useState(false);
+
     const { settings, sum, rate } = props;
 
     const formatter = new Intl.NumberFormat('ru-RU', { style: 'currency', currency: (settings.appCurrency ? settings.appCurrency : "CAD") });
@@ -44,22 +50,29 @@ function TableHeading(props) {
 
 //! hidden currency exchange btn and 2 more lines for lists totals but they must be changed as well
 
-    return <div className="totals flex  justify-between bg-blue-100 px-8 py-6">
-                <div className="total-group flex flex-col items-end">
+    return <div className={`totals flex justify-between bg-blue-100 px-8 py-6 relative ${isExpanded ? 'flex-col' : ''}`}>
+                <div className={`total-group flex items-end ${isExpanded ? 'justify-between' : 'flex-col'}`}>
                     <p className="totals budget">
                         Budget: 
                     </p>
                     <p className='text-2xl'>{formatter.format(normalizedBudget())}</p>
                 </div>
 
-                <p className="totals sum hidden">
-                    Before: <span>{formatter.format(sum.before)}</span>
-                </p>
-                <p className="totals sum hidden">
-                    After: <span>{formatter.format(sum.after)}</span>
-                </p>
+                <div className={`total-group flex items-end ${isExpanded ? 'justify-between' : 'hidden'}`}>
+                    <p className={`totals sum `}>
+                        Before: 
+                    </p>
+                    <p>{formatter.format(sum.before)}</p>
+                </div>
 
-                <div className="total-group flex flex-col items-end">
+                <div className={`total-group flex items-end ${isExpanded ? 'justify-between' : 'hidden'}`}>
+                    <p className={`totals sum`}>
+                        After: 
+                    </p>
+                    <p>{formatter.format(sum.after)}</p>
+                </div>
+
+                <div className={`total-group flex items-end ${isExpanded ? 'justify-between' : 'flex-col'}`}>
                     <p className={"totals sum-result " + (budgetResult >= 0 ? "" : "excel")}>
                         Total: 
                     </p>
@@ -67,6 +80,8 @@ function TableHeading(props) {
                         {formatter.format(budgetResult)}
                     </p>
                 </div>
+
+
                 
 
                 {/* <div className="currency-change" >
@@ -80,6 +95,22 @@ function TableHeading(props) {
                     <button className={"currency-button " + (settings.appCurrency === "rub" ? "active" : "")} onClick={props.onSwitch} >RUR</button>
                 </div> */}
     
+                <div className="absolute -bottom-4  left-0 right-0 flex justify-center">
+                    {isExpanded 
+                        ? <button 
+                            onClick={ () => setIsExpanded(false)} 
+                            className="w-8 h-8 rounded-2xl border-gray-300 border-2 bg-white text-gray-400">
+                                <ExpandLessIcon />
+                            </button> 
+                        : <button 
+                            onClick={ () => setIsExpanded(true)}
+                            className="w-8 h-8 rounded-2xl border-gray-300 border-2 bg-white text-gray-400">
+                                <ExpandMoreIcon />
+                            </button>
+                    }
+                    
+                    
+                </div>
             </div>
 }
 
